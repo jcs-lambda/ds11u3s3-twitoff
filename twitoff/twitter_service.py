d34cf37065cd1d8957cb385a00361040b6e6e75c
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-02-25 14:08:20 -0600

    rename environment variable

--- twitoff/twitter_service.py
+++ twitoff/twitter_service.py
@@ -8,13 +8,13 @@ from dotenv import load_dotenv
 # establish environment
 assert load_dotenv(), 'failed to initialize environment'
 TWITTER_API_KEY = os.getenv('TWITTER_API_KEY')
-TWITTER_API_SECRET_KEY = os.getenv('TWITTER_API_SECRET_KEY')
+TWITTER_API_KEY_SECRET = os.getenv('TWITTER_API_KEY_SECRET')
 TWITTER_ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
 TWITTER_ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
 assert TWITTER_API_KEY is not None, \
     'falied to load TWITTER_API_KEY from environment'
-assert TWITTER_API_SECRET_KEY is not None, \
-    'falied to load TWITTER_API_SECRET_KEY from environment'
+assert TWITTER_API_KEY_SECRET is not None, \
+    'falied to load TWITTER_API_KEY_SECRET from environment'
 assert TWITTER_ACCESS_TOKEN is not None, \
     'falied to load TWITTER_ACCESS_TOKEN from environment'
 assert TWITTER_ACCESS_TOKEN_SECRET is not None, \
@@ -22,7 +22,7 @@ assert TWITTER_ACCESS_TOKEN_SECRET is not None, \
 
 
 def twitter_api():
-    auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET_KEY)
+    auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_KEY_SECRET)
     auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
     return api
